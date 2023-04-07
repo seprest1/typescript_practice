2226c3bc3561d46de03813fb62dcd1a549c79fe7
Sarah Preston <sarahpreston@Sarahs-MacBook-Pro.local>
2023-04-07 13:37:45 -0500

    section 2/3

---
+++ annotations/variables.ts
@@ -0,0 +1,50 @@
+const apples: number = 5;
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+//Classes
+class Car{
+
+}
+let car: Car = new Car();
+
+//Object literal
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20
+};
+
+//Function 
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+};
+
+const json = `{ "x": 10, "y": 20 }`;
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); //{ x:10, y: 20 };
+
+
+// 2) When we declare a variable on one line
+// and initialize it later
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++){
+    if (words[i] === 'green'){
+        foundWord = true;
+    }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] > 0){
+        numberAboveZero = numbers[i];
+    }
+}
--- index.ts
+++ index.ts
@@ -1,27 +1,39 @@
 import axios from "axios";
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+const today = new Date();
+today.getMonth();
 
-interface Todo {
-    id: number;
-    title: string;
-    completed: boolean;
+const person = {
+    age: 20
 };
 
-axios.get(url).then(response => {
-    const todo = response.data as Todo;
+class Color {}
+const red = new Color();
 
-    const id = todo.id;
-    const title = todo.title;
-    const completed = todo.completed;
+
+
+// const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// interface Todo {
+//     id: number;
+//     title: string;
+//     completed: boolean;
+// };
+
+// axios.get(url).then(response => {
+//     const todo = response.data as Todo;
+
+//     const id = todo.id;
+//     const title = todo.title;
+//     const completed = todo.completed;
     
-    logTodo(id, title, completed);
-});
+//     logTodo(id, title, completed);
+// });
 
-const logTodo = (id: number, title: string, completed: boolean) => {
-    console.log(` 
-    The todo with ID: ${id}
-    Has a title of: ${title}
-    Is it completed? ${completed}
-`);
-};
+// const logTodo = (id: number, title: string, completed: boolean) => {
+//     console.log(` 
+//     The todo with ID: ${id}
+//     Has a title of: ${title}
+//     Is it completed? ${completed}
+// `);
+// };
